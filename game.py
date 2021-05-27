df809b1df0616348bf813fcfdc449b7ca7a714d1
brianstauffer <brian.m.stauffer@gmail.com>
2021-05-27 18:24:58 -0400

    Determine the winner

--- game.py
+++ game.py
@@ -6,9 +6,9 @@ import random
 print("Rock, Paper, Scissors, Shoot!")
 
 
-user_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
+user_choice = input("Please choose one of 'rock', 'paper', 'scissors':")
 
-print("USER CHOICE: ", user_choice)
+print("USER CHOICE:", user_choice)
 
 # validate the input such that only if it is one of hte expected values
 # ...will we continue with the rest of the program
@@ -27,5 +27,32 @@ else:
 valid_options = ["rock", "paper", "scissors"]
 computer_choice = random.choice(valid_options)
 print("COMPUTER CHOICE: ", computer_choice)
+
+# determine who won!
+
+
+# adapted from code shared by Reid in Slack
+
+#USER WINNING AND TIE SCENARIOS
+if user_choice == computer_choice:
+    print("It's a tie, try again!")
+
+elif (user_choice == "scissors") and (computer_choice == "paper"):
+    print("Scissors cuts paper, you win!")
+elif (user_choice == "rock") and (computer_choice == "scissors"):
+    print("rock smashes scissors, you win!")
+elif (user_choice == "paper") and (computer_choice == "rock"):
+    print("Paper covers rock, you win!")
+
+#COMPUTER WINNING SCENARIOS
+elif (user_choice == "scissors") and (computer_choice == "rock"):
+    print("rock smashes scissors, you lose!")
+elif (user_choice == "rock") and (computer_choice == "paper"):
+    print("paper covers rock, you lose!")
+elif (user_choice == "paper") and (computer_choice == "scissors"):
+    print("Scissors cuts paper, you lose!")
+
+# configure player name via env variables
+
 
 print("THIS IS THE END OF OUR GAME, PLEASE PLAY AGAIN")
