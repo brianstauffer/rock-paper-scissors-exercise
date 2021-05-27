f5919f5265c0ca1b2e9332f3fb6d3782f4239ed9
brianstauffer <brian.m.stauffer@gmail.com>
2021-05-27 18:34:31 -0400

    Use another approach, both valid

--- game.py
+++ game.py
@@ -31,26 +31,29 @@ print("COMPUTER CHOICE: ", computer_choice)
 # determine who won!
 
 
-# adapted from code shared by Reid in Slack
+# adapted from code shared by Jan in Slack
 
-#USER WINNING AND TIE SCENARIOS
-if user_choice == computer_choice:
-    print("It's a tie, try again!")
-
-elif (user_choice == "scissors") and (computer_choice == "paper"):
-    print("Scissors cuts paper, you win!")
-elif (user_choice == "rock") and (computer_choice == "scissors"):
-    print("rock smashes scissors, you win!")
-elif (user_choice == "paper") and (computer_choice == "rock"):
-    print("Paper covers rock, you win!")
-
-#COMPUTER WINNING SCENARIOS
-elif (user_choice == "scissors") and (computer_choice == "rock"):
-    print("rock smashes scissors, you lose!")
-elif (user_choice == "rock") and (computer_choice == "paper"):
-    print("paper covers rock, you lose!")
-elif (user_choice == "paper") and (computer_choice == "scissors"):
-    print("Scissors cuts paper, you lose!")
+if user_choice == "rock":
+    if computer_choice == "rock":
+        print("IT'S A TIE")
+    elif computer_choice == "paper":
+        print("OH, THE COMPUTER WON...")
+    elif computer_choice == "scissors":
+        print("YOU WON! CONGRATS!")
+elif user_choice == "paper":
+    if computer_choice == "rock":
+        print("YOU WON! CONGRATS!")
+    elif computer_choice == "paper":
+        print("IT'S A TIE")
+    elif computer_choice == "scissors":
+        print("OH, THE COMPUTER WON...")
+elif user_choice == "scissors":
+    if computer_choice == "rock":
+        print("OH, THE COMPUTER WON...")
+    elif computer_choice == "paper":
+        print("YOU WON! CONGRATS!")
+    elif computer_choice == "scissors":
+        print("IT'S A TIE")
 
 # configure player name via env variables
 
